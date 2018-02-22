7a6592d4e1a6c166d1b4b39c31b783b2e7de8ddd
Domingos Latorre <domingos.latorre@gmail.com>
2018-02-22 10:10:38 -0300

    Exercício Semana 3

---
+++ Modulo1/Semana3/Exercicios/Ex01.php
@@ -0,0 +1,12 @@
+/**
+* --------------------------------------------------------------------------
+* Módulo 1 / Semana 3 / Exercício 1
+* --------------------------------------------------------------------------
+* Crie uma função que recebe como parâmetro dois números inteiros e
+* na saída a seguinte mensagem:
+*  O Resultado da Soma é: ?
+*  O Resultado da Subtração é: ?
+*  O Resultado da Multiplicação é: ?
+*  O Resultado da Divisão é: ?
+* --------------------------------------------------------------------------
+*/
